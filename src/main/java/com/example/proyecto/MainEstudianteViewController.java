b34f39336ceaebdafebf6f05cb128b0ce90fd14c
Alisson <100102366+AlissonCamposMar@users.noreply.github.com>
2025-04-28 00:09:29 -0500

    Empieza el desarrollo sobre el usuario estudiante

---
+++ src/main/java/com/example/proyecto/MainEstudianteViewController.java
@@ -0,0 +1,6 @@
+package com.example.proyecto;
+
+public class MainEstudianteViewController {
+    //CONSULTA PARA MOSTRAR TODOS LOS DATOS DEL ESTUDIANTE
+    //SELECT ID_USUARIO, NOMBRE, CORREO FROM USUARIO WHERE tipo_usuario = 'Estudiante';
+}
