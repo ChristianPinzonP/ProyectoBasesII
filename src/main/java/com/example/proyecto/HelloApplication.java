3949dd0191cfc5eca5f3c81ac2a67ebdf3bc2129
Christian Pinzon <ccpinzonp@uqvirtual.edu.co>
2025-05-09 09:36:23 -0500

    Se organizo la vista del home

--- src/main/java/com/example/proyecto/HelloApplication.java
+++ src/main/java/com/example/proyecto/HelloApplication.java
@@ -29,7 +29,7 @@ public class HelloApplication extends Application {
             e.printStackTrace();
         }
     }
-
+//
     private void initRootLayout() {
         try {
             rootLayout = new BorderPane();
